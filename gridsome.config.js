6e6af2a8f94f48a3eb9e9fe1475765aaea71019f
Raphael-D <31120433+Raphael-D@users.noreply.github.com>
2019-12-18 15:39:09 +0900

    test

--- gridsome.config.js
+++ gridsome.config.js
@@ -14,6 +14,7 @@ module.exports = {
       options: {
         baseUrl: process.env.WORDPRESS_URL, // required
         typeName: 'WordPress', // GraphQL schema name (Optional)
+        apiBase: 'wp-json'
       }
     }
   ]
